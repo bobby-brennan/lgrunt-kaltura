a28de05ab7574373dee35baed81df3953966548c
EC2 Default User <ec2-user@ip-172-31-14-135.us-west-2.compute.internal>
2015-01-11 17:55:19 +0000

    Add support for https

--- src/static/js/media-list.js
+++ src/static/js/media-list.js
@@ -1,11 +1,11 @@
 app.controller('MediaListController', function($scope) {
   $scope.loadScript = function(video) {
-    $.getScript("http://cdnapi.kaltura.com/p/" +
+    $.getScript("https://cdnapisec.kaltura.com/p/" +
       video.partnerId  + "/sp/" +  video.partnerId + "00/" +
       "embedIframeJs/uiconf_id/27577942/partner_id/" + video.partnerId +
       "?autoembed=true&entry_id=" + video.id +
       "&playerId=kaltura_player_" + video.id +
-      "&cache_st=1420925754&width=400&height=333")
+      "&width=400&height=333")
     .then(function() {console.log('loaded script')},
           function(err) {console.log('err:' + err)})
   }
